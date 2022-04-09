a2e55b7dbdd09543d9729b915b9bf7c674e7911b
Castle <castlerock88@protonmail.com>
2022-04-08 21:03:47 -0400

    trying to add discord bot

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 require("dotenv").config({ path: "./config/.env" });
 const cron = require("node-cron");
+const token = process.env.TOKEN;
+
+const { Client, Intents } = require("discord.js");
 
 const connectDB = require("./config/database");
 const addData = require("./services/data-interactions");
 
 connectDB();
 
-// addData();
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+// Login to Discord with your client's token
+client.login(token);
 
 cron.schedule("0 * * * *", () => {
   addData();
